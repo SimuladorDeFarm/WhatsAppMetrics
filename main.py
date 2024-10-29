cf5f9c38253d07b8a4e8690539c20e4043709447
tomas <tomasfariasp123@gmail.com>
2024-10-29 12:49:44 -0300

    add function validar datos

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from modules.files.load_file import load_file
 from modules.files.convert_txt_to_csv import file_to_list
+from modules.files.convert_txt_to_csv import line_validator
+
 
 file = load_file()
 
-
+print(line_validator(file))
 
 file.close() 
--- modules/files/convert_txt_to_csv.py
+++ modules/files/convert_txt_to_csv.py
@@ -1,10 +1,34 @@
-
+import re
 
 def file_to_list(file):
     
-    linea = file.readlines()
+    line = file.readlines()
     
-    return linea
+    return line
 
+def msj_line_validator(text):
+    
+    # validation hh
+    match = re.search(r"^\d{1,2}/\d{1,2}/\d{4}, \d{2}:\d{2} -.+:.+$", text)
+    
+    if match == None:
+        return False
+    else:
+        return True
 
+def line_validator(file):
+
+    lines = file_to_list(file)
+
+    i = 0    
+    while i < len(lines):
+
+        if msj_line_validator(lines[i]) == False:
+            del lines[i]
+            i-=1
+        
+        i+=1
+
+    return lines
+    
 
